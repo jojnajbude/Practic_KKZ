7539e3ae7bc09e3b3d581828a9d94ac01749c43a
jojnajbude <98945425+jojnajbude@users.noreply.github.com>
2022-05-31 13:59:55 +0300

    add class Complex
    Класс где описаны копмлексные числа.

---
+++ prac1_console/prac1_console/Complex.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac1_console
+{
+    internal class Complex
+    {
+        private int _a = 0;
+        private int _b = 0;
+        private int _i;
+
+        public int Re { set => _a = value; get => _a; }
+        public int Im { set => _b = value; get => _b; }
+
+        public Complex()
+        {
+
+        }
+
+        public Complex(int a, int b)
+        {
+            _a = a;
+            _b = b;
+        }
+
+        ~Complex()
+        {
+            Console.WriteLine("Object has destroyed.");
+        }
+
+        
+        public static Complex operator +(Complex u, Complex v)
+        {
+            u._a += v._a;
+            u._b += v._b;
+            return u;
+        }
+
+        public static Complex operator -(Complex u, Complex v)
+        {
+            u._a -= v._a;
+            u._b -= v._b;
+            return u;
+        }
+
+        public static Complex operator *(Complex u, Complex v)
+        {
+            u._a = (u._a * v._a) - (u._b * v._b);
+            v._b = (u._b * v._a) + (u._a * v._b);
+            return u;
+        }
+
+        public static Complex operator /(Complex u, Complex v)
+        {
+            u._a = ((u._a * v._a) + (u._b * v._b)) / (v._a^2 + v._b^2);
+            v._b = ((u._b*v._a)-(u._a*v._b)) / (v._a ^ 2 + v._b ^ 2);
+            return u;
+
+        }
+
+        public static Complex Parse(string str)
+        {
+            Complex res = new Complex();
+            string[] str_array = str.Split(' ');
+            res._a = Convert.ToInt32(str_array[0]);
+            res._b = Convert.ToInt32(str_array[2].Trim('i'));
+            return res;
+        }
+        
+        
+
+        public override string ToString()
+        {
+            if (_b > 0) return _a + "+" + _b + "i";
+            else if (_b == 0) return Convert.ToString(_a);
+            else if (_a == 0) return _b + "i";
+            else if (_b < 0) return _a +""+ _b + "i";
+            else return "Error";
+        }
+
+    }
+}
--- prac1_console/prac1_console/Program.cs
+++ prac1_console/prac1_console/Program.cs
@@ -2,39 +2,12 @@
 
 namespace prac1_console
 {
-
-
    
+
+
     internal class Program
     {
         static void Main(string[] args)
-        {
-            //string s1 = "1 5 12 10";
-            //string s2 = "123/10";
-
-
-            //Fraction s = Fraction.Parse(s1);
-            //Console.WriteLine(s.IntPart + "|" + s.Numerator + "/" + s.Denominator);
-            ///*
-            //Fraction f = new Fraction(38, 12);
-            //f.GetMixedView();
-            //Console.WriteLine(f.IntPart+"|"+f.Numerator+"/"+f.Denominator);*/
-
-            Fraction s = new Fraction(-5.123456789);
-            Console.WriteLine(s);
-
-            /*
-            Time t1 = new Time(156489);
-            Time t2 = new Time(33, 15, 12);
-            Time t3 = new Time(60,75,12);
-            Time t4 = new Time();
-            Console.WriteLine(t3);
-            Console.WriteLine(t1);
-            t3 = t3 - t1;
-            Console.WriteLine(t3);
-            Console.WriteLine(Time.Parse("15 62 13"));
-            Console.WriteLine(Time.Parse("156489"));*/
-
-        }
+        {}
     }
 }
