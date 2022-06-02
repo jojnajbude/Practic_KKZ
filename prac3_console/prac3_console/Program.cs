c2292184ee8d7cf9ca1a4c4df694a08339ce735a
JackFordTrY <jackford35241@gmail.com>
2022-06-02 12:14:38 +0300

    prac3 created

---
+++ prac3_console/prac3_console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace prac3_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
