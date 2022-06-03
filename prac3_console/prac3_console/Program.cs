a0bd04689f763fba160bd403fe87568327ee2882
JackFordTrY <jackford35241@gmail.com>
2022-06-03 10:55:17 +0300

    Classes structure added

--- prac3_console/prac3_console/Program.cs
+++ prac3_console/prac3_console/Program.cs
@@ -2,11 +2,30 @@
 
 namespace prac3_console
 {
+    public abstract class Organization
+    {
+
+    }
+
+    public class Factory: Organization
+    {
+
+    }
+
+    public class Insurance: Organization
+    {
+
+    }
+
+    public class BuildingCompany: Organization
+    {
+
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
