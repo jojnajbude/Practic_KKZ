64b5db1988b52619f702d8b48c737480a4aebd17
JackFordTrY <jackford35241@gmail.com>
2022-06-08 12:14:28 +0300

    prac5

---
+++ prac5_console/Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task21
+{
+    enum Shapes
+    {
+        Square, 
+        Circle,
+        Triangle
+    }
+
+    class Picture
+    {
+        Shape[] shapes;
+        int _arraySize;
+
+        public Picture(int arraySize)
+        {
+            _arraySize = arraySize;
+        }
+    }
+
+    abstract class Shape
+    {
+        public abstract string Figure{get; set;}
+            
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ prac5_console/prac5_console/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace prac5_console
+{
+    interface IPrint
+    {
+        public void Print();
+    }
+
+    abstract class GeometricShape
+    {
+        public abstract float GetArea();
+    }
+
+    class Rectangle: GeometricShape, IPrint
+    {
+        private float _sideA, _sideB;
+
+        public float SideA { get { return _sideA; } set { _sideA = value; } }
+        public float SideB { get { return _sideB; } set { _sideB = value; } }
+
+        public override float GetArea()
+        {
+            return _sideA * _sideB;  
+        }
+
+        public override string ToString()
+        {
+            return $"Side A: {_sideA}\nSide B: {_sideB}\nArea: {GetArea()}";
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(this);
+        }
+
+        public Rectangle(float sideA, float sideB)
+        {
+            _sideA = sideA;
+            _sideB = sideB;
+        }
+    }
+
+    class Square: Rectangle
+    {
+        public override string ToString()
+        {
+            return $"Side: {SideA}\nArea: {GetArea()}";
+        }
+
+        public Square(float sideA) : base(sideA, sideA)
+        {
+
+        }
+    }
+
+    class Circle: GeometricShape, IPrint
+    {
+        float _radius;
+        
+        public float Radius { get { return _radius; } set { _radius = value; } }
+
+        public override float GetArea()
+        {
+            return (float)Math.PI * _radius * _radius;
+        }
+
+        public override string ToString()
+        {
+            return $"Radius: {Radius}\nArea: {GetArea()}";
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(this);
+        }
+
+        public Circle(float radius)
+        {
+            _radius = radius;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle= new Circle(4.6f);
+            circle.Print();
+            Console.WriteLine();
+            
+            Rectangle rectangle = new Rectangle(4, 5);
+            rectangle.Print();
+            Console.WriteLine();
+            
+            Square square = new Square(8);
+            square.Print();
+        }
+    }
+}
